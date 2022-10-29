ff29cc16e02daa3fb72cce8a2db2ff87e31ac821
saimall <rameshwaramsaimallik@gmail.com>
2022-10-29 19:50:10 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 2000
 app.listen(port, () => {
   console.log(`server listening at port - ${port}`);
 });
